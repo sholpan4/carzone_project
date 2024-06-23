166c6286a1d4aae2c026eda4ba1b28b7ff015f36
sholpan4 <b.o.sholpan44@gmail.com>
2024-06-23 13:03:31 +0500

    completed teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,6 +125,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'carzone/static',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255, db_index=True, verbose_name='Firstname')
+    last_name = models.CharField(max_length=255, db_index=True, verbose_name='Lastname')
+    designation = models.CharField(max_length=255, verbose_name='Designation')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True, verbose_name='Photo')
+    facebook_link = models.URLField(max_length=100, blank=True, null=True)
+    twitter_link = models.URLField(max_length=100, blank=True, null=True)
+    google_link = models.URLField(max_length=100, blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Created Date')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
